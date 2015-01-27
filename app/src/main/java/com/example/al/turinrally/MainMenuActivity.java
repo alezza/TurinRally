31481eb98a89a45aa264589a69177f6e185d2b3d
Lidia-Andra Lezza <lezzaandra@gmail.com>
2015-01-27 20:45:52 +0100

    committing changes

--- app/src/main/java/com/example/al/turinrally/MainMenuActivity.java
+++ app/src/main/java/com/example/al/turinrally/MainMenuActivity.java
@@ -1,10 +1,13 @@
 package com.example.al.turinrally;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -50,12 +53,54 @@ public class MainMenuActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 Uri webpage = Uri.parse("http://en.wikipedia.org/wiki/Turin");
                 intent.setData(webpage);
                 intent.setPackage("org.wikipedia");
-                startActivity(intent);
+
+                // the wiki app will open only if it is installed
+                // otherwise it will take the user to the play store to download it from there
+                try {
+                    startActivity(intent);
+                }
+                catch (android.content.ActivityNotFoundException e){
+
+                    Log.d("exception", "i am in catch 1" + MainMenuActivity.this);
+
+                    // 1. Instantiate an AlertDialog.Builder with its constructor
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage(R.string.dialog_fire_missiles)
+                            .setTitle(R.string.wiki_missing_app);
+
+                    // Add the buttons
+                    // this button sends you to download the wiki app in case you don't have it
+                    builder.setPositiveButton(R.string.ok_wiki_button, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK button
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=org.wikipedia")));
+                        }
+                    });
+
+                    // this button cancels the request to download the wiki app
+                    builder.setNegativeButton(R.string.cancel_wiki_button, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+
+
+                    // 3. Get the AlertDialog from create()
+                    AlertDialog dialog = builder.create();
+
+                    dialog.show();
+
+                    Log.d("exception","i am in catch 2");
+                }
+
             }
         });
     }
@@ -66,8 +111,44 @@ public class MainMenuActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, GameActivity.class);
-                startActivity(intent);
+
+                // the wiki app will open only if it is installed
+                // otherwise it will take the user to the play store to download it from there
+                try {
+                    Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.duxgames.snake");
+                    startActivity(LaunchIntent);
+                }
+                catch (NullPointerException e){
+
+                    // 1. Instantiate an AlertDialog.Builder with its constructor
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage(R.string.game_dialog_box)
+                            .setTitle(R.string.game_missing_app);
+
+                    // Add the buttons
+                    // this button sends you to download the wiki app in case you don't have it
+                    builder.setPositiveButton(R.string.ok_wiki_button, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK button
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.duxgames.snake")));
+                        }
+                    });
+
+                    // this button cancels the request to download the wiki app
+                    builder.setNegativeButton(R.string.cancel_wiki_button, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+
+
+                    // 3. Get the AlertDialog from create()
+                    AlertDialog dialog = builder.create();
+
+                    dialog.show();
+                }
             }
         });
     }
